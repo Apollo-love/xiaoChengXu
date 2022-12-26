6b0f3b92021162ea5e91e0fa221c69886b35b35e
gaotenglong <1368465491@qq.com>
2022-12-26 12:36:42 +0800

    商品列表页面开发

---
+++ components/good-list/good-list.vue
@@ -0,0 +1,59 @@
+<template>
+  <view class="box">
+    <view class="left">
+      <image :src="objs.goods_small_logo" ></image>
+    </view>
+    <view class="right">
+      <view>{{objs.goods_name}}</view>
+      <view class="text">￥{{objs.goods_price | tofix}}</view>
+      
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"good-list",
+    data() {
+      return {
+        
+      };
+    },
+    props:{
+      objs:{
+        type:Object
+      }
+    },
+    filters:{
+      tofix(value){
+        return parseFloat(value).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.box{
+    display: flex;
+    border: 1px solid #efefef;
+    box-shadow: 10px 10px 10px #efefef;
+    margin: 10rpx 0;
+    .left{
+      margin-right: 20rpx;
+      image{
+        width: 280rpx;
+        height: 280rpx;
+        
+      }
+    }
+    .right{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+       .text{
+         color: red;
+         font-size: 18px;
+       }
+      }
+    }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
-  <view>
-    <view>1</view>
+  <view class="content">
+    <view v-for="(obj,i) in goodsList" :key="i" @click="gotoGoodsList(obj)">
+     <good-list :objs="obj"></good-list>
+    </view>
   </view>
 </template>
 
@@ -8,12 +10,58 @@
   export default {
     data() {
       return {
-        
+        queryObj:{
+          query:'',
+          cid:'',
+          pagenum:1,
+          pagesize:10
+        },
+        goodsList:[],
+        total:0,
+        isloading:false
       };
+    },
+    methods:{
+      async getGoodsList(cb){
+        this.isloading=true
+       const {data:res}=await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+       console.log(res,'21')
+       this.total=res.message.total
+       this.goodsList=[...this.goodsList,...res.message.goods]
+       cb&&cb()
+       this.isloading=false
+      },
+      gotoGoodsList(value){
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id='+value.goods_id
+        })
+      }
+    },
+    onLoad() {
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      this.goodsList=[]
+      this.queryObj.pagenum=1
+      this.isloading=false
+      this.getGoodsList(()=>{
+        uni.stopPullDownRefresh()
+      })
+    },
+    onReachBottom(){
+      console.log(1)
+      if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total) return uni.$showMsg('数据加载完毕')
+      if(this.isloading) return uni.$showMsg('正在加载中...')
+      this.queryObj.pagenum+=1
+      this.getGoodsList()
     }
   }
 </script>
 
 <style lang="scss">
-
+  .content{
+    padding: 0 20rpx;
+  }
+  
+  
 </style>
